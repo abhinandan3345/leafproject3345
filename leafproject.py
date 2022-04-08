934f77fa8b8ea4716a41b4bf905410f8e1976122
abhinandan3345 <65812192+abhinandan3345@users.noreply.github.com>
2022-04-08 13:11:35 +0530

    Update leafproject.py

--- leafproject.py
+++ leafproject.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
-from keras.preprocessing.image import load_img,img_to_array
+tf.keras.preprocessing.image.load_img
+tf.keras.utils.img_to_array
 import numpy as np
 from keras.models import load_model
 import requests
